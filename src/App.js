e0d2e30cd7c78056a0efd2fb3f1b3d5eaaba1ab4
Angel Cazares <acazares979@gmail.com>
2022-04-02 03:01:08 -0500

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponents';
+import { Component } from 'react';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className='container'>
-          <NavbarBrand href="/">
-            Ristorante Con Fusion
-          </NavbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
+
+class App extends Component{
+  render(){
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className='container'>
+            <NavbarBrand href="/">
+              Ristorante Con Fusion
+            </NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
